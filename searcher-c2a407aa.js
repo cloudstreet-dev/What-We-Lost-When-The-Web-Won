d44ec8806d764223b28dc7c03571b530e8ab00bf
DavidLiedle <DavidLiedle@users.noreply.github.com>
2026-05-12 20:51:11 +0000

    deploy: b57c55e071770abee4f7388624402194c828d838

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-63ba934d.js',
+                path_to_root + 'searchindex-6763ea9b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
